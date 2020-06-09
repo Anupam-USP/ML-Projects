f89c9527d5a1288bd07a97366f27e8006104b3cd
anupam <theanuempire1509@gmail.com>
2020-06-09 21:56:22 +0530

    Added new predict function

---
+++ Movie Recommendation System/Movie_predict.py
@@ -0,0 +1,44 @@
+import warnings
+
+import pandas as pd
+
+warnings.filterwarnings('ignore')
+
+# Get the dataset
+column_names = ['user_id', 'item_id', 'rating', 'timestamp']
+# Give your path to dataset
+df = pd.read_csv("/home/anupam/Downloads/ml-100k/u.data",
+                 sep='\t', names=column_names)
+
+movies_title = pd.read_csv(
+    "/home/anupam/Downloads/ml-100k/u.item", sep='\|', header=None)
+
+movies_title = movies_title[[0, 1]]
+movies_title.columns = ['item_id', 'title']
+
+df = pd.merge(df, movies_title, on='item_id')
+
+# Exploratory data analysis
+ratings = pd.DataFrame(df.groupby('title').mean()['rating'])
+ratings['no. of ratings'] = pd.DataFrame(df.groupby('title').count()['rating'])
+
+movie_matrix = df.pivot_table(
+    index="user_id", columns="title", values="rating")
+
+
+def predict(movie_name):
+    movie_user_ratings = movie_matrix[movie_name]
+    similar_to_movie = movie_matrix.corrwith(movie_user_ratings)
+
+    corr_movie = pd.DataFrame(similar_to_movie, columns=['correlation'])
+    corr_movie.dropna(inplace=True)
+
+    corr_movie = corr_movie.join(ratings['no. of ratings'])
+    prediction = corr_movie[corr_movie['no. of ratings']
+                            > 100].sort_values('correlation', ascending=False)
+    print(prediction.head())
+    return prediction
+
+
+movie_name = input()
+prediction = predict(movie_name)
